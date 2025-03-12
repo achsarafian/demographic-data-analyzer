09e359a88ad7182b998db297288a86b5ae33b20a
Achilleas Sarafianos <achsarafian@gmail.com>
2025-03-12 17:27:10 +0000

    answered two more questions and rounded answers to two nearest decimal points

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -10,22 +10,23 @@ def calculate_demographic_data(print_data=True):
 
     # What is the average age of men?
     mask = df['sex'] == 'Male'
-    average_age_men = df[mask].age.mean()
+    average_age_men = round(df[mask].age.mean(), 2)
 
     # What is the percentage of people who have a Bachelor's degree?
     mask = df['education'] == 'Bachelors'
-    percentage_bachelors = 100 * ( df[mask].shape[0] / df.shape[0] )
+    percentage_bachelors = round(100 * (df[mask].shape[0] / df.shape[0]), 2)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    mask = (df['education'] == 'Bachelors') | (df['education'] == 'Masters') | (df['education'] == 'Doctorate')
+    higher_education = df[mask]
+    lower_education = df[~mask]
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = round(100 * (higher_education[higher_education['salary'] == '>50K'].shape[0] / higher_education.shape[0]), 2)
+    lower_education_rich = round(100 * (lower_education[lower_education['salary'] == '>50K'].shape[0] / lower_education.shape[0]), 2)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
     min_work_hours = None
